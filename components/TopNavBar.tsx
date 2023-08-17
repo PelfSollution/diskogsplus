b35485f50ee79f347a5d63750ecf9c0213dadf96
David A <daayros@protonmail.com>
2023-08-17 17:55:49 +0200

    cambiar img > image + exportar componente dashboard para vercel (error)

--- components/TopNavBar.tsx
+++ components/TopNavBar.tsx
@@ -1,4 +1,4 @@
-
+import Image from 'next/image';
 import { Button } from "@/components/ui/button"
 
 interface TopNavBarProps {
@@ -12,7 +12,7 @@ export default function TopNavBar({ isLoggedIn, onLogout }: TopNavBarProps) {
     return (
         <nav className="w-full bg-white shadow-md py-2 px-4 fixed top-0 left-0 z-10">
         <div className="container mx-auto flex justify-between items-center">
-          <img src="/diskogs-logo.gif" alt="Diskogs plus" className="w-24" />
+          <Image src="/diskogs-logo.gif" alt="Diskogs plus" className="w-24" />
           <ul className="flex space-x-4 items-center">
             <li className="cursor-pointer inline-flex items-center">Opción 1</li>
             <li className="cursor-pointer inline-flex items-center">Opción 2</li>
---
+++ pages/dashboard.tsx
@@ -0,0 +1,6 @@
+function Dashboard() {
+    return <div>Contenido del Dashboard</div>;
+  }
+  
+  export default Dashboard;
+  

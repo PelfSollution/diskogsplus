32f7ddec9438364d8732b3f1497183f33839e0ab
David A <daayros@protonmail.com>
2023-08-18 20:03:27 +0200

    Implementa hook useGetUserData con SWR y actualiza el panel del usuario (Dashboard)

---
+++ hooks/useGetUserData.ts
@@ -0,0 +1,36 @@
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+interface UserObjectInterface {
+  id: number;
+  username: string;
+  resource_url: string;
+  consumer_name: string;
+  location: string;
+  profile: string;
+  registered: string;
+  num_collection: number;
+  avatar_url: string;
+  favorite_styles?: string;
+}
+
+export interface UserIdentityDataProps {
+  data: {
+    userProfile: UserObjectInterface;
+  };
+  error: boolean | undefined;
+  isLoading: boolean | undefined;
+  isValidating: boolean | undefined;
+}
+
+console.log(`Fetching from: ${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/identity`);
+
+const useGetUserData = () => {
+  const { data, error, isLoading, isValidating }: UserIdentityDataProps =
+    useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/identity`, fetcher);
+  return { data, error, isLoading, isValidating };
+};
+
+export default useGetUserData;
+
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,27 +1,47 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import TopNavBar from "@/components/top-nav-bar"
+import { getCookie } from 'cookies-next';
+import useGetUserData from '@/hooks/useGetUserData';
+import TopNavBar from "@/components/top-nav-bar";
 
 function Dashboard() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
     const router = useRouter();
+    const username = getCookie('username');
+    const { data, error, isLoading, isValidating } = useGetUserData();
 
     const handleLogout = () => {
         setIsLoggedIn(false);
-
-        // Redirigir al usuario a la página raíz
         router.push('/');
     }
 
     return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <TopNavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-        <header className="text-center mb-8">
-        { isLoggedIn && 
-            <p className="text-xl">Loggin Realizado! - INFO DE USUARIO</p>
-        }
-        </header>
-    </div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+            <TopNavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+
+            <header className="text-center mb-8">
+                {isLoggedIn && 
+                    <div>
+                        <p className="text-xl">Loggin Realizado!</p>
+                        
+                        {error && !isLoading && <div>Error: failed to load</div>}
+                        {isLoading || isValidating && <div>Loading...</div>}
+
+                        {data?.userProfile && Object.keys(data).length > 0 ? (
+                            <div>
+                                <h1>Hola, {data.userProfile.username}</h1>
+                                {data.userProfile.avatar_url && <img src={data.userProfile.avatar_url} alt={`${data.userProfile.username} profile pic`} />}
+                                <p>Registrado: {data.userProfile.registered}</p>
+                                <p>Tu tienes {data.userProfile.num_collection} discos en tu colecci&oacute;n.</p>
+
+                            </div>
+                        ) : (
+                            <p>Inicie sesi&oacute;n para ver su perfil.</p>
+                        )}
+                    </div>
+                }
+            </header>
+        </div>
     );
 }
 

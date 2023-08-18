c425ff5780d1ed81b32317d9efeb5a1f3795e9d3
David A <daayros@protonmail.com>
2023-08-18 18:09:16 +0200

    Añadir funcion de API "identity" para obtener y desencriptar la información del usuario a partir de cookies

--- pages/api/auth/identity.ts
+++ pages/api/auth/identity.ts
@@ -3,112 +3,52 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getCookie } from 'cookies-next';
 var CryptoJS = require('crypto-js');
 
-// import { User } from '../../../db/models';
-
-//INTERFACE
-export interface FavoriteAlbumInterface {
-  title: string;
-  artist: string;
-}
-
-//Function grabs our user's data
-//It needs the accessData object, which is stored encrypted as a cookie
 export default async function identity(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // function mode(arr: FavoriteAlbumInterface[]) {
-  //   return arr
-  //     .sort(
-  //       (a, b) =>
-  //         arr.filter((v) => v.title === a.title).length -
-  //         arr.filter((v) => v.title === b.title).length
-  //     )
-  //     .pop();
-  // }
   try {
-    //get cookies
+    // Obtener cookies cifradas
     const usernameCipher = getCookie('usernameCipher', { req, res });
     const accessDatacipherObj = getCookie('accessData', { req, res });
 
+    // Si no existen las cookies, responder con un objeto vacío
     if (!usernameCipher || !accessDatacipherObj) {
-      res.send({ userProfile: {}, mostListened: {} });
+      return res.send({ userProfile: {} });
     }
 
-    //Unencrypt accessData cookie object
-    const accessBytes = await CryptoJS.AES.decrypt(
+    // Desencriptar la cookie de datos de acceso
+    const accessBytes = CryptoJS.AES.decrypt(
       accessDatacipherObj,
       process.env.CRYPT_KEY
     );
+    const accessData = JSON.parse(accessBytes.toString(CryptoJS.enc.Utf8));
 
-    const accessData = await JSON.parse(
-      accessBytes.toString(CryptoJS.enc.Utf8)
-    );
-
-    //unencrypt username
-    const usernameBytes = await CryptoJS.AES.decrypt(
+    // Desencriptar la cookie del nombre de usuario
+    const usernameBytes = CryptoJS.AES.decrypt(
       usernameCipher,
       process.env.CRYPT_KEY
     );
+    const username = JSON.parse(usernameBytes.toString(CryptoJS.enc.Utf8));
 
-    const username = await JSON.parse(
-      usernameBytes.toString(CryptoJS.enc.Utf8)
-    );
-
+    // Si no existen el nombre de usuario o los datos de acceso, responder con un objeto vacío
     if (!username || !accessData) {
-      res.send({ userProfile: {}, mostListened: {} });
+      return res.send({ userProfile: {} });
     }
 
-    // const user = await User.findOne({
-    //   where: { username: username },
-    // });
-
-    var dis = await new Discogs(accessData);
+    // Conectar con Discogs y obtener el perfil de usuario
+    var dis = new Discogs(accessData);
     const userProfile = await dis.user().getProfile(username);
 
-    ////////////
-    //MOST LISTENED FUNCTIONALITY WIP
-    ////////////
-
-    // const userId = (user as any).id ? (user as any).id : 0;
-    try {
-      //   const albumArray = await Album.findAll({
-      //     where: { user_id: userId },
-      //     raw: true,
-      //     order: [['created_at', 'DESC']],
-      //     include: [
-      //       {
-      //         model: Cartridge,
-      //         attributes: ['name'],
-      //         as: 'albumCartridge',
-      //       },
-      //     ],
-      //   });
-
-      let mostListened = null;
-
-      // if (albumArray.length > 0) {
-      //   const albumMode = mode(albumArray);
-      //   console.log(albumMode, albumArray.indexOf(albumMode));
-      //   if (
-      //     albumArray.indexOf(albumMode) !==
-      //     albumArray.lastIndexOf(albumMode)
-      //   ) {
-      //     mostListened = albumMode;
-      //   }
-      // }
-
-      // console.log(mostListened);
-
-      if (userProfile) {
-        res.send({ userProfile, mostListened });
-      } else {
-        res.status(401).json({ error: 'Error. No such user.' });
-      }
-    } catch (e) {
-      console.log(e);
+    // Responder con el perfil de usuario
+    if (userProfile) {
+      return res.send({ userProfile });
+    } else {
+      return res.status(401).json({ error: 'Error. No such user.' });
     }
   } catch (err) {
-    res.send(err);
+    console.error(err);
+    return res.status(500).send(err);
   }
 }
+

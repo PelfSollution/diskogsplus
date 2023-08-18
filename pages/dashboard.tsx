e2586f660ab0b3b07c34f0943ef870cdfe14b113
David A <daayros@protonmail.com>
2023-08-18 21:00:19 +0200

    Mejoras en la presentación del dashboard con tailwindcss y formateo de fecha con date-fns

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { getCookie } from 'cookies-next';
+import format from 'date-fns/format';
 import useGetUserData from '@/hooks/useGetUserData';
+import { Button } from "@/components/ui/button"
 import TopNavBar from "@/components/top-nav-bar";
 
 function Dashboard() {
@@ -15,6 +17,7 @@ function Dashboard() {
         router.push('/');
     }
 
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
             <TopNavBar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
@@ -22,18 +25,16 @@ function Dashboard() {
             <header className="text-center mb-8">
                 {isLoggedIn && 
                     <div>
-                        <p className="text-xl">Loggin Realizado!</p>
-                        
                         {error && !isLoading && <div>Error: failed to load</div>}
                         {isLoading || isValidating && <div>Loading...</div>}
 
                         {data?.userProfile && Object.keys(data).length > 0 ? (
                             <div>
-                                <h1>Hola, {data.userProfile.username}</h1>
-                                {data.userProfile.avatar_url && <img src={data.userProfile.avatar_url} alt={`${data.userProfile.username} profile pic`} />}
-                                <p>Registrado: {data.userProfile.registered}</p>
-                                <p>Tu tienes {data.userProfile.num_collection} discos en tu colecci&oacute;n.</p>
-
+                                <h1 className='text-2xl'>Hola, <span className="font-bold text-blue-500">{data.userProfile.username}</span></h1>
+                                {data.userProfile.avatar_url && <img className='mt-4 rounded-full object-cover' src={data.userProfile.avatar_url} alt={`${data.userProfile.username} profile pic`} />}
+                                <p className='mt-4'><span className="font-bold">Registrado:</span> {format(new Date(data.userProfile.registered), 'dd/MM/yyyy HH:mm')}</p>
+                                <p>Tu tienes <span className="font-bold text-blue-500">{data.userProfile.num_collection}</span> discos en tu colecci&oacute;n.</p>
+                                <Button onClick={() => router.push('/albums')} className="mt-4 self-center">Ver Discos</Button>
                             </div>
                         ) : (
                             <p>Inicie sesi&oacute;n para ver su perfil.</p>

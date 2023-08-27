de8251fca329081d1ca86fd01f649d92933a5c42
David A <daayros@protonmail.com>
2023-08-27 21:16:21 +0200

    Correcciones de advertencias antes de despliegue en Vercel

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## DiscogsPlus
-
+```
 diskogsplus/
 ┣ .next/
 ┃ ┣ cache/
@@ -62,6 +62,7 @@ diskogsplus/
 ┣ tailwind.config.js
 ┣ tailwind.config.ts
 ┗ tsconfig.json
+```
 
 ## Getting Started
 
--- pages/mixtape.tsx
+++ pages/mixtape.tsx
@@ -1,52 +1,24 @@
 import { useQuery } from "convex/react";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
 import { api } from "../convex/_generated/api";
 
-export default function Mixtape() {
+export default function Convex() {
 
-  
-  
-  const mixtape = useQuery(api.mixtapes.get);
   const tracks = useQuery(api.tracks.get);
-
-  if (!mixtape || !tracks) return null;
-
-const trackMap: Record<string, { artist: string; trackTitle: string }> = tracks.reduce((acc, track) => {
-  acc[track._id] = track;
-  return acc;
-}, {});
-
-
-
-  
-
-  console.log(mixtape);
   console.log(tracks);
-
   return (
     <Layout>
-    <main className="flex min-h-screen flex-col items-center p-24">
-      {mixtape.map(({ _id, mixtapeName, tracks: trackIds }) => (
-        <div key={_id.toString()}>
-          <h2 className="text-xl font-bold mb-4">{mixtapeName}</h2>
+      <main className="flex min-h-screen flex-col items-center p-24">
+  
+        <h2 className="text-xl font-bold mb-4">MIXTAPE</h2>
 
-          {/* Listado de tracks */}
-          <ul>
-            {trackIds.map((trackId: string, index: number) => {
-              const track = trackMap[trackId];
-              if (track) { // Verificamos que track existe en el mapa
-                return (
-                  <li key={index}>Tema: {track.artist} - {track.trackTitle}</li>
-                );
-              }
-              return null; // Si no existe, no renderizamos nada
-            })}
-          </ul>
-        </div>
-      ))}
-    </main>
-  </Layout>
+        {/* Listado de tracks */}
+        <ul>
+          {tracks && tracks.map(({trackTitle, artist}, index) => ( // <-- Check if tracks is defined
+            <li key={index}>{artist} - {trackTitle}</li>
+          ))}
+        </ul>
+      </main>
+    </Layout>
   );
 }

085bd7e87596a766bdb78080bbf665312f14daca
David A <daayros@protonmail.com>
2023-09-01 12:09:59 +0200

    !!! Eliminación completa de Convex y preparación para migración a Supabase.

--- convex/getMixtape.ts
+++
@@ -1,6 +0,0 @@
-import { query } from './_generated/server'
-import { Doc } from './_generated/dataModel'
-
-export const list = query(async (ctx): Promise<Doc<"mixtape">[]> => {
-  return await ctx.db.query("mixtape").collect();
-});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
-if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
-  throw new Error("NEXT_PUBLIC_CONVEX_URL no está definido en las variables de entorno.");
-}
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ConvexProvider client={convex}>
   <Component {...pageProps} />
-  </ConvexProvider>
   );
 }
 

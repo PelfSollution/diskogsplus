201b6d947a04d81d3a600706b73a40bcebf9f0b5
David A <daayros@protonmail.com>
2023-09-09 11:30:23 +0200

    Corrección de entidades no escapadas en JSX del matching al deployar en Vercel

--- pages/matching.tsx
+++ pages/matching.tsx
@@ -156,7 +156,7 @@ const handleCloseSnackbar = () => {
           <div>
             <p className="tw-mb-4 tw-mt-4">
               Discos que <span className="tw-font-bold">{user2}</span> tiene y tu{" "}
-              <span className="tw-font-bold">"{user1}"</span> no tienes:{" "}
+              <span className="tw-font-bold">&quot;{user1}&quot;</span> no tienes:{" "}
               <span className="tw-font-bold tw-text-blue-500">
                 {albumsDifference[0].length}
               </span>

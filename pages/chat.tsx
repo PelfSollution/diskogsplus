6b1d3e2c0f28b52c32ccd149230a10317691adea
David A <daayros@protonmail.com>
2023-09-13 00:08:14 +0200

    Styling chat [test]

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,31 +1,45 @@
 import Layout from "@/components/Layout";
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
+import { TextField, Typography, Button } from "@mui/material";
 
 export default function Chat() {
-    const { messages, handleSubmit, input, handleInputChange } = useChat();
-    return (
-      <Layout
-        title="Chat - Discogs Plus"
-        description="Chat"
-        centeredContent={true}
-      >
-        <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
-            <form onSubmit={handleSubmit}>
-            <label htmlFor="input">Prompt</label>
-
-      <input
-        name="prompt"
-        value={input}
-        onChange={handleInputChange}
-        id="input"
-      />
-      <button type="submit">Submit</button>
+  const { messages, handleSubmit, input, handleInputChange } = useChat();
+  return (
+    <Layout
+      title="Chat - Discogs Plus"
+      description="Chat"
+      centeredContent={true}
+    >
+      <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
       {messages.map((message, i) => (
-        <div key={i}>{message.content}</div>
-      ))}
-    </form>
-    </div>
-      </Layout>
-    );
-  }
-  
+            <div
+              key={i}
+              className="tw-font-sm tw-bg-gray-200 tw-p-4 tw-rounded-md tw-mb-2 tw-max-w-[800px] tw-min-w-[800px]"
+            >
+              <Typography variant="body2" color="textSecondary">
+                {message.content}
+              </Typography>
+            </div>
+          ))}
+        <form onSubmit={handleSubmit} className="tw-mt-4">
+   
+          <div className="tw-flex tw-flex-col tw-gap-2">
+            <TextField
+              label="Prompt"
+              variant="outlined"
+              onChange={handleInputChange}
+              value={input}
+              className="tw-mr-8"
+              id="input"
+              size="small"
+            />
+
+            <Button type="submit" variant="outlined">
+              Enviar
+            </Button>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
+}

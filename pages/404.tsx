9445579dc7b59f8ba66a7e93ac8f1bcd067b8bba
David A <daayros@protonmail.com>
2023-09-22 20:01:55 +0200

    Mejora y Styling en las páginas 404 y _error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,28 @@
 import Layout from "@/components/Layout";
+import Image from 'next/image';
 
 const Custom404 = () => (
   <Layout
     centeredContent={true}
     title="404 - Diskogs +"
-    description="404: Página no encontrada"
+    description="404 - Page Not Found"
   >
-    <div className="tw-text-center"> {/* Añadido para centrar el texto horizontalmente */}
-      <h1>404 - Página no encontrada</h1>
+    <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
+      <header className="tw-text-center tw-max-w-xl mx-auto">
+        <div className="tw-flex tw-flex-col tw-items-center tw-justify-center">
+          <h1 className="tw-text-4xl tw-text-red-400">404 - Page Not Found</h1> {/* Aquí aumenté el tamaño del texto */}
+          
+          <Image 
+            src="/img-404.png" 
+            alt="404"
+            className="tw-w-auto tw-max-w-md mx-auto block"
+            width={300}
+            height={300}
+          />
+          <p className="tw-text-xl tw-mt-2">Oops! Looks like this vinyl is broken. We couldn't find what you were looking for.
+          </p>
+        </div>
+      </header>      
     </div>
   </Layout>
 );
--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,13 +1,37 @@
 import { NextPageContext } from 'next';
 import Layout from "@/components/Layout";
+import Image from 'next/image';
 
 interface ErrorProps {
   statusCode: number;
 }
 
 const ErrorPage = ({ statusCode }: ErrorProps) => (
-  <Layout title={`Error ${statusCode}`} centeredContent={true}>
-    <h1>{statusCode ? `Error ${statusCode}` : "Ha ocurrido un error"}</h1>
+  <Layout
+    centeredContent={true}
+    title={`Error ${statusCode} - Diskogs +`}
+    description={`Error ${statusCode}: Página no encontrada`}
+  >
+    <div className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
+      <header className="tw-text-center tw-max-w-xl mx-auto">
+        <div className="tw-flex tw-flex-col tw-items-center tw-justify-center">
+          <h1 className="tw-text-4xl">
+            {statusCode ? `Error ${statusCode} - Page Not Found` : "An error has occurred"}
+          </h1>
+          
+          <Image 
+            src="/img-404.png" 
+            alt="Error"
+            className="tw-w-auto tw-max-w-md mx-auto block"
+            width={300}
+            height={300}
+          />
+          <p className="tw-text-xl tw-mt-2">
+            Oops! Looks like this vinyl is broken. We couldn't find what you were looking for or an unexpected error occurred.
+          </p>
+        </div>
+      </header>      
+    </div>
   </Layout>
 );
 
@@ -17,3 +41,4 @@ ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
 };
 
 export default ErrorPage;
+
